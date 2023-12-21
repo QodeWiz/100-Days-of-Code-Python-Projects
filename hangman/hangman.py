dad71cb3d74555bdb2ea6fdce9a2baefcbf82e60
ishwiz <154478861+ishwiz@users.noreply.github.com>
2023-12-21 19:05:33 +1100

    in built choice func used

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,8 +4,7 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-random_index = random.randint(0, len(word_list)-1)
-chosen_word = word_list[random_index]
+chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Enter your guess: ").lower()
