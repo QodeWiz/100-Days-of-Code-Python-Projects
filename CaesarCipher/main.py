9768accc5e96155ed74ad7a77927db968488f6f3
Ishrat <ikau0004@student.monash.edU>
2023-12-22 13:21:52 +1100

    encode method, to encrypt forward

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -6,23 +6,15 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
+    encoded_word = []
     for letter in text:
-        for letter in alphabet:
-            if letter == letter:
-                print(alphabet.index(letter))
-        
-
-encrypt(text, shift)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+        current_index = alphabet.index(letter)
+        new_index = current_index + shift
+        if new_index > len(alphabet)-1:
+            new_index = new_index - len(alphabet)
+        encoded_letter = alphabet[new_index]
+        encoded_word.append(encoded_letter)
+    print(''.join(encoded_word))
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(text, shift)
