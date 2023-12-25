1c69c182677c3282cdeec449a900a88fc4fa0d81
ishwiz <154478861+ishwiz@users.noreply.github.com>
2023-12-25 16:07:05 +1100

    secret auction project complete

--- BeginnerProjects/TheSecretAuction/main.py
+++ BeginnerProjects/TheSecretAuction/main.py
@@ -1,32 +1,32 @@
-# committing my plan:
+import os
+from art import logo
 
-#                  ┌──────────────────────────────────────────────┐
-#                  │                                              │
-# ┌───────────────►│ get user input - name and bid                │
-# │                │ store user bid in a dictionary               │
-# │                │                                              │
-# │                └──────────────────────────────────────────────┘
-# │
-# │
-# │                ┌──────────────────────────────────────────────┐
-# │                │                                              │
-# │                │ ask user, if there are more bidders          │
-# │                │                                              │
-# │                └─────┬────────────────────────────────────┬───┘
-# │                      │                                    │
-# │                      │                                    │
-# │                      │                                    │
-# │                      │                                    │
-# │   yes                │                                    │                 no
-# │                      │                                    │
-# └──────────────────────┘                                    └────────────────────────┐
-#                                                                                      │
-#                                                                                      │
-#                                                                                      │
-#                                                                                      │
-#                                                                                      ▼
-#                                                                        ┌───────────────────────────────┐
-#                                                                        │  Loop throught he dictionary  │
-#                                                                        │  to find the highest bid      │
-#                                                                        │                               │
-#                                                                        └───────────────────────────────┘
+print(logo)
+welcome_message = ("Welcome to the Secret Auction!")
+print(welcome_message)
+
+secretAuctionBids = {}
+
+def theSecretAuction():
+    moreParticipants = True
+    while moreParticipants:
+        name = input("Your name is: ")
+        bid = int(input("Your bid is: $"))
+        moreBidders = input("Are there any more bidders? Type 'yes' or 'no': ")
+        os.system('clear')
+
+        secretAuctionBids[name] = bid
+
+        if moreBidders == "no":
+            moreParticipants = False
+    
+    list_of_bids = []
+    for participant in secretAuctionBids:
+        list_of_bids.append(secretAuctionBids[participant])
+
+    highest_bid = max(list_of_bids)
+    highest_bidder = list(secretAuctionBids.keys())[list(secretAuctionBids.values()).index(highest_bid)]
+
+    print(f'The highest bid is {highest_bidder} with a bid of ${highest_bid}')
+
+theSecretAuction()
