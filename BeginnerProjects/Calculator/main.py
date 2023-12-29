127210b59770a35a63c109dfce20eef6df660a79
ishwiz <154478861+ishwiz@users.noreply.github.com>
2023-12-29 19:17:26 +1100

    recursion of calculator function

--- BeginnerProjects/Calculator/main.py
+++ BeginnerProjects/Calculator/main.py
@@ -21,15 +21,39 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+def calculator():
+    num1 = int(input("What's the first number?: "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
 
-operation_symbol = input("Pick a symbol: ")
+    operation_symbol = input("Pick a symbol: ")
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+    num2 = int(input("What's the next number?: "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    moreCalculations = True
+
+    while moreCalculations:
+        toContinue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        if toContinue == 'y':
+            operation_symbol = input("Pick a symbol: ")
+            
+            num3 = int(input("What's the next number?: "))
+
+            calculation_function = operations[operation_symbol] 
+            next_answer = calculation_function(answer, num3)
+
+            print(f"{answer} {operation_symbol} {num3} = {next_answer}")
+            answer = next_answer
+        
+        else:
+            moreCalculations = False
+
+    calculator()
+
+calculator()
