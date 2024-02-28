a040b17c432a29329fd2ef048c7f47d86e602d4a
Ishrat <ikau0004@student.monash.edU>
2024-02-28 11:45:51 +1100

    blackjack capstone project

--- BeginnerProjects/BlackjackCapstoneProject/main.py
+++ BeginnerProjects/BlackjackCapstoneProject/main.py
@@ -1,18 +1,18 @@
 from art import logo
 import random
+
 startGame = input("Do you want to play a game of BlackJack? Type 'y' or 'n': ")
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 
 def winCondition(player_score, computer_score):
-    if player_score > 21:
-        print("You went over! You lose")
-    elif computer_score > 21:
-        print("Opponent went over! You win")
-    elif player_score < computer_score:
-        print("You lose")
+    if player_score <= 21 and computer_score <= 21:
+        if player_score < computer_score:
+            print("Opponent is closer to 21! You lose")
+        else: 
+            print("You are closer to 21! You win")
     else:
-        print("You win")
+        print("You went over! You lose")
 
 def blackjackGame():
     playerCards = random.sample(cards, 2)
@@ -27,12 +27,17 @@ def blackjackGame():
         computer_current_score += card
     print(f"Testing computer cards: {computerCards}, current score = {computer_current_score}")
     print(f"Computer's first card: {computerCards[0]}")
-    while computer_current_score <= 16:
+    if computer_current_score == 21 and len(computerCards) == 2:
+        return 0
+    if computer_current_score <= 17 and computer_current_score > 0:
         ComputerNextCard = random.choice(cards)
         computerCards.append(ComputerNextCard)
         computer_current_score += ComputerNextCard
         print(f"Testing computer cards: {computerCards}, current score = {computer_current_score}")
-
+    if 11 in computerCards and sum > 21:
+        computerCards.remove(11)
+        computerCards.append(1)
+    computer_current_score = sum(computerCards)    
     shouldContinue = True
 
     while shouldContinue:
@@ -46,6 +51,7 @@ def blackjackGame():
         else:
             shouldContinue = False
             winCondition(player_score=player_current_score, computer_score=computer_current_score)
+
 
 if startGame == 'y':
     print(logo)
